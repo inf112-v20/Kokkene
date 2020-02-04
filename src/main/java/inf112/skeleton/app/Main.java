cb2d1aebbbe471dcaeffb2fa2b787861c7d37278
Mikmus <Markus.jensen51@gmail.com>
2020-02-04 16:01:51 +0100

    Added code for visualization of map, some classes that are not being used, has been removed.

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -8,11 +8,11 @@ public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
         cfg.title = "RoboRally";
-        cfg.width = 1080;
-        cfg.height = 720;
+        cfg.width = 500;
+        cfg.height = 500;
 
-        Options options = new Options();
-        new LwjglApplication(new MainMenu(options), cfg);
+        RoboRally rr = new RoboRally();
+        new LwjglApplication(rr, cfg);
 
     }
 }
--- src/main/java/inf112/skeleton/app/RoboRally.java
+++ src/main/java/inf112/skeleton/app/RoboRally.java
@@ -4,24 +4,51 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.Map;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class RoboRally implements ApplicationListener {
-    private SpriteBatch batch;
-    private BitmapFont font;
+
+    TiledMap map;
+    TiledMapTileLayer boardLayer;
+    TiledMapTileLayer playerLayer;
+    TiledMapTileLayer holeLayer;
+    TiledMapTileLayer flagLayer;
+
+    OrthogonalTiledMapRenderer mapRenderer;
+    OrthographicCamera camera;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        font = new BitmapFont();
-        font.setColor(Color.RED);
+
+        map = new TmxMapLoader().load("fiveTiles.tmx");
+        boardLayer = (TiledMapTileLayer) map.getLayers().get("Board");
+        playerLayer = (TiledMapTileLayer) map.getLayers().get("Player");
+        holeLayer = (TiledMapTileLayer) map.getLayers().get("Hole");
+        flagLayer = (TiledMapTileLayer) map.getLayers().get("Flag");
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 5, 5);
+        camera.position.x = 2.5f;
+        camera.update();
+
+
+        float unitScale = 1/300f;
+        mapRenderer = new OrthogonalTiledMapRenderer(map, unitScale);
+        mapRenderer.setView(camera);
+
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        font.dispose();
+        mapRenderer.dispose();
+        map.dispose();
     }
 
     @Override
@@ -29,9 +56,8 @@ public class RoboRally implements ApplicationListener {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        batch.begin();
-        font.draw(batch, "RoboRally", 500, 500);
-        batch.end();
+        mapRenderer.render();
+
     }
 
     @Override
