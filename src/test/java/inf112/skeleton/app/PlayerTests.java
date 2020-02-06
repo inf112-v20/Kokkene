6c10b5a575794b212effb4b5f781bcf8b675c8d6
ArthurApps <60078221+ArthurApps@users.noreply.github.com>
2020-02-06 14:47:07 +0100

    Made PlayerTests Updated DizzyDash in main/assets

---
+++ src/test/java/inf112/skeleton/app/PlayerTests.java
@@ -0,0 +1,32 @@
+package inf112.skeleton.app;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class PlayerTests {
+
+    Player player;
+    int xPos, yPos, orientation = 1;
+
+    @Before
+    public void MakePlayer() {
+        player = new Player("player", xPos, yPos, orientation);
+    }
+
+    @Test
+    public void PlayerHasTenHealthAfterInitiation() {
+        assertEquals(player.getHealth(), 10);
+    }
+
+    @Test
+    public void PlayerHasThreeLivesAfterInitiation() {
+        assertEquals(player.getLifePoints(),3);
+    }
+
+    public void PlayerHasOrientationOne() {
+        assertEquals(player.getOrientation(),1);
+    }
+}
