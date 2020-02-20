5a455f728a070dea913c0a76dd8589735f3c5686
Martin Johnsen <johnsen.martin1@gmail.com>
2020-02-20 15:49:08 +0100

    added hearts as healthpoints

--- src/main/java/inf112/skeleton/app/HUD.java
+++ src/main/java/inf112/skeleton/app/HUD.java
@@ -1,6 +1,8 @@
 package inf112.skeleton.app;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -12,18 +14,23 @@ public class HUD {
 
     SpriteBatch batch = new SpriteBatch();
     BitmapFont font = new BitmapFont();
+    Texture heart = new Texture(Gdx.files.internal("heart.png"));
 
     public HUD(Player player) {
         this.player = player;
         font.setColor(Color.RED);
         font.getData().setScale(2,2);
 
+
     }
 
-    // renders a HUD staticly on the game window.
+    // renders a HUD staticly on the top of the game window.
     public void render() {
         batch.begin();
-        font.draw(batch, "HP: " + Integer.toString(player.getHealth()), WIDTH-100, HEIGHT-10);
+        //font.draw(batch, "HP: " + Integer.toString(player.getHealth()), WIDTH-100, HEIGHT-10);
+        for(int i = 0; i < player.getHealth(); i++) {
+            batch.draw(heart, (i*25), HEIGHT-40, 45,45);
+        }
         batch.end();
     }
 }
