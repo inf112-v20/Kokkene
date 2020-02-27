e0d9b70e76e279ae07eaf6578a51d5b8df1f2027
Martin Johnsen <johnsen.martin1@gmail.com>
2020-02-27 18:13:43 +0100

    Added title text to menu

--- src/main/java/inf112/skeleton/app/Menu.java
+++ src/main/java/inf112/skeleton/app/Menu.java
@@ -3,13 +3,17 @@ package inf112.skeleton.app;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+import java.awt.*;
 
 public class Menu implements Screen {
 
@@ -21,6 +25,12 @@ public class Menu implements Screen {
 
     public static String mapFile;
 
+    private BitmapFont font;
+    private SpriteBatch batch;
+
+    private int height = Gdx.graphics.getHeight();
+    private int width = Gdx.graphics.getWidth();
+
     //private Texture background;
 
     Game game;
@@ -30,9 +40,11 @@ public class Menu implements Screen {
 
         sb = new SpriteBatch();
         stage = new Stage();
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        font.setColor(Color.RED);
+        font.getData().setScale(4);
 
-        int height = Gdx.graphics.getHeight();
-        int width = Gdx.graphics.getWidth();
 
         //Start Game button
         gameButton = new Button(new TextureRegionDrawable(new TextureRegion(new Texture("assets/pictures/button.png"))));
@@ -62,13 +74,24 @@ public class Menu implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         /*
-        * sb.begin();
-        * sb.draw(background, 0, 0, width, height);
-        * sb.end();
-        */
+         * sb.begin();
+         * sb.draw(background, 0, 0, width, height);
+         * sb.end();
+         */
 
         stage.act(delta);
         stage.draw();
+        batch.begin();
+
+        //draw start game title on gamebutton
+        font.draw(batch, "Start Game!", width/2-(gameButton.getWidth()/2) + 40,
+                (height /2f)-(gameButton.getHeight()/2));
+
+        //draw exit title on button
+        font.draw(batch, "Exit game!", width/2-(exitButton.getWidth()/2) + 40,
+                (height/4));
+
+        batch.end();
 
         if(gameButton.isPressed()){
             LoadGame();
@@ -76,6 +99,7 @@ public class Menu implements Screen {
         if(exitButton.isPressed()) {
             Gdx.app.exit();
         }
+
 
     }
 
