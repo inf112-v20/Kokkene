89de7da616a2a9310211c7ff727270225eb98fd4
Jan Kåre Sandblåst <Jan.Sandblast@student.uib.no>
2020-02-28 13:48:49 +0100

    Fixed typo and improved readability in HUD
    Also fixed warning about integer division in float method

--- src/main/java/inf112/skeleton/app/HUD.java
+++ src/main/java/inf112/skeleton/app/HUD.java
@@ -25,19 +25,22 @@ public class HUD {
         font.getData().setScale(2,2);
     }
 
-    // renders a HUD staticly on the top of the game window.
+    // renders a HUD statically on the top of the game window.
     public void render() {
         batch.begin();
-        int tileX = Display.getWidth()/board.cardWidth;
-        int tileY = Display.getHeight()/board.cardHeight;
         int heartSize = Display.getWidth()/40;
         //font.draw(batch, "HP: " + player.getHealth(), WIDTH-100, HEIGHT-10);
         for(int i = 0; i < player.getHealth(); i++) {
-            batch.draw(heart, (i*25), HEIGHT-40, heartSize,heartSize);
+            batch.draw(heart, (i*25), HEIGHT-40, heartSize, heartSize);
         }
         int lifeP = player.getLifePoints();
+        int tileX = Display.getWidth()/board.cardWidth;
+        int tileY = Display.getHeight()/board.cardHeight;
         for(int i = lifeP; 0 < i; i--) {
-            batch.draw(heart, ((player.getxPos()+1)*tileX)-((tileX/(lifeP+1))*i)-heartSize/2, player.getyPos()*tileY+heartSize/2, heartSize, heartSize);
+            batch.draw(heart,
+                    ((player.getxPos()+1)*tileX)-((tileX/(lifeP+1f))*i)-heartSize/2f,
+                    player.getyPos()*tileY+heartSize/2f,
+                    heartSize, heartSize);
         }
         font.draw(batch, "Objective: " + player.getObjective(),275, HEIGHT-10);
         batch.end();
