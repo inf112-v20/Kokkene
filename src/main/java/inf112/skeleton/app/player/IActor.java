b8c093f4969870ab3a19b64716018924f0783060
Kristoffer Kraft Johansen <Kristoffer.K.Johansen@student.uib.no>
2020-04-14 14:55:28 +0200

    Created an Interface for actors on the board called "IActor.java"

---
+++ src/main/java/inf112/skeleton/app/player/IActor.java
@@ -0,0 +1,232 @@
+package inf112.skeleton.app.player;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import inf112.skeleton.app.objects.Card;
+import inf112.skeleton.app.objects.Deck;
+
+import java.util.ArrayList;
+import java.util.List;
+
+interface IActor {
+
+    //Constant Fields
+    int MAXHEALTH = 10;
+
+    /**
+     * Get the PlayerState containing the various textures for the actor
+     *
+     * @return PlayerState of this actor
+     */
+    PlayerState getPlayerState();
+
+    /**
+     * Set the PlayerState of this actor
+     *
+     * @param ps PlayerState to store in a field in this actor
+     */
+    void setPlayerState(PlayerState ps);
+
+    /**
+     * Get the PlayerState containing the textures of the healthbars
+     *
+     * @return PlayerState of HealthBars of this actor
+     */
+    PlayerState getHealthBars();
+
+    /**
+     * Set the PlayerState containing the textures of the healthbars
+     *
+     * @param hb PlayerState of HealthBars to store in a field in this actor
+     */
+    void setHealthBars(PlayerState hb);
+
+    /**
+     * Get whether the actor is done selecting cards and thus is ready to play
+     *
+     * @return whether actor is ready
+     */
+    boolean getReady();
+
+    /**
+     * Set the readiness of the actor to given boolean value b
+     *
+     * @param b boolean value to set the ready field to
+     */
+    void setReady(boolean b);
+
+    /**
+     * Toggles whether ready or not
+     */
+    boolean toggleReady();
+
+    /**
+     * The current position is the new backup
+     */
+    void newBackup();
+
+    /**
+     * @return respawn x-position
+     */
+    int getxBackup();
+
+    /**
+     * @return respawn y-position
+     */
+    int getyBackup();
+
+    /**
+     * @return name of the specific actor.
+     */
+    String getName();
+
+    /**
+     * @return ID of specific actor
+     */
+    int getId();
+
+    /**
+     * @return the current x Position for this the actor.
+     */
+    int getxPos();
+
+    /**
+     * Replace x position with a new one
+     *
+     * @param xPos new x position
+     */
+    void setxPos(int xPos);
+
+    /**
+     * @return the current y position for this the actor.
+     */
+    int getyPos();
+
+    /**
+     * @param yPos  the y-value this the actor should have.
+     */
+    void setyPos(int yPos);
+
+    /**
+     * @return current rotation in a 0-3 scale.
+     */
+    int getOrientation();
+
+    /**
+     * Sets the direction
+     * @param orientation  orientation is the direction in a 0-3 scale.
+     */
+    void setOrientation(int orientation);
+
+    /**
+     * Turns the the actor
+     * @param change positive numbers are to the left, negative turns to the right
+     */
+    void turn(int change);
+
+    /**
+     *
+     * @return max health of the actor
+     */
+    int getMaxHealth();
+
+    /**
+     *
+     * @return current health total.
+     */
+    int getHealth();
+
+    /**
+     * @param health The amount
+     */
+    void addHealth(int health);
+
+    /**
+     * @return the amount of lifePoints
+     */
+    int getLifePoints();
+
+    /**
+     * @return true if the actor is alive
+     */
+    boolean isAlive();
+
+    /**
+     * Resets the actors position to the current backup
+     */
+    void resetPos();
+
+    /**
+     * @return the next objective to stand at to get points
+     */
+    int getObjective();
+
+    /**
+     * @param ob  new objective
+     */
+    void checkObjective(int ob);
+
+    /**
+     * @param texture for the sprite of a actor
+     * @return true if the actor is alive
+     */
+    TextureRegion[][] setPlayerTextures (String texture);
+
+    /**
+     * How many cards you may select in this round
+     * @return # of cards to select
+     */
+    int cardsToSelect();
+
+    /**
+     * How many registers are locked such that one cannot change the cards
+     * @return # of locked registers
+     */
+    int lockedRegisters();
+
+    /**
+     * Lock the last selected card if damaged such that the register must lock
+     */
+    void lockRegister();
+
+    /**
+     * gives cards to actor based on how much health is left
+     * @param deck used to get cards from
+     */
+    void setHand(Deck deck);
+
+    /**
+     *
+     * @return returns the cards the current actor is holding
+     */
+    Card[] getCards();
+
+    /**
+     * Toggles whether a card is selected or not
+     *
+     * @param c is the card we toggle
+     */
+    void toggleCard(Card c);
+
+    /**
+     * Get the list of currently selected cards
+     * @return list of cards
+     */
+    List<Card> getSelected();
+
+    /**
+     * Get the list of currently locked cards, this list will be in reverse order of execution
+     * @return list of locked cards
+     */
+    ArrayList<Card> getLocked();
+
+    /**
+     * Discards all current cards except the ones locked, and draw a new hand
+     * @param deck deck to draw from and return the cards to
+     */
+    void discardDraw(Deck deck);
+
+    /**
+     * Respawn function to be called after each round, will set position to backup coords and remove 1 life
+     */
+    void respawn();
+}
--- src/main/java/inf112/skeleton/app/player/Player.java
+++ src/main/java/inf112/skeleton/app/player/Player.java
@@ -21,7 +21,7 @@ import java.util.List;
  *
  */
 
-public class Player {
+public class Player implements IActor {
 
     //ID of the robot
     private String name;
@@ -58,12 +58,7 @@ public class Player {
     //Ready to play selected cards
     private boolean ready;
 
-    //Constant variables
-    private final int MAXHEALTH = 10;
-
     public boolean playerPower = false;
-
-    //Player constructor had to be like this for testing
 
     /**
      * @param name  the name for this robot.
@@ -83,145 +78,72 @@ public class Player {
         this.ready = false;
     }
 
-    /**
-     * Get the PlayerState containing the various textures for the player
-     *
-     * @return PlayerState of this player
-     */
     public PlayerState getPlayerState() {
         return ps;
     }
 
-    /**
-     * Set the PlayeState of this player
-     *
-     * @param ps PlayerState to store in a field in this player
-     */
     public void setPlayerState(PlayerState ps) {
         this.ps = ps;
     }
 
-    /**
-     * Get the PlayerState containing the textures of the healthbars
-     *
-     * @return PlayerState of HealthBars of this player
-     */
     public PlayerState getHealthBars() {
         return hb;
     }
 
-    /**
-     * Set the PlayerState containing the textures of the healthbars
-     *
-     * @param hb PlayerState of HealthBars to store in a field in this player
-     */
     public void setHealthBars(PlayerState hb) {
         this.hb = hb;
     }
 
-    /**
-     * Get whether the player is done selecting cards and thus is ready to play
-     *
-     * @return whether player is ready
-     */
     public boolean getReady() {
         return this.ready;
     }
 
-    /**
-     * Set the readiness of the player to given boolean value b
-     *
-     * @param b boolean value to set the ready field to
-     */
     public void setReady(boolean b) {
         this.ready = b;
     }
 
-    /**
-     * Toggles whether ready or not
-     */
     public boolean toggleReady() {
         this.ready = !this.ready;
         return this.ready;
     }
 
-    /**
-     * The current position is the new backup
-     */
     public void newBackup() {
         this.xBackup = this.xPos;
         this.yBackup = this.yPos;
     }
 
-    /**
-     * @return respawn x-position
-     */
     public int getxBackup() { return xBackup; }
 
-    /**
-     * @return respawn y-position
-     */
     public int getyBackup() {
         return yBackup;
     }
 
-    /**
-     * @return name of the specific robot.
-     */
     public String getName() {
         return name;
     }
 
-    /**
-     * @return ID of specific robot
-     */
     public int getId() {
         return id;
     }
 
-    /**
-     * @return the current x Position for this robot.
-     */
     public int getxPos() {
         return xPos;
     }
 
-    /**
-     * Replace x position with a new one
-     *
-     * @param xPos new x position
-     */
     public void setxPos(int xPos) { this.xPos = xPos; }
 
-    /**
-     * @return the current y position for this robot.
-     */
     public int getyPos() { return yPos; }
 
-    /**
-     * @param yPos  the y-value this robot should have.
-     */
     public void setyPos(int yPos) { this.yPos = yPos; }
 
-    /**
-     * @return current rotation in a 0-3 scale.
-     */
     public int getOrientation() {
         return orientation % 4;
     }
 
-    /**
-     * Sets the direction
-     * @param orientation  orientation is the direction in a 0-3 scale.
-     */
     public void setOrientation(int orientation) {
         this.orientation = Math.abs(orientation)%4;
     }
 
-    /**
-     * Turns the robot
-     * @param change positive numbers are to the left, negative turns to the right
-     */
     public void turn(int change){
         change += getOrientation();
 
@@ -232,17 +154,112 @@ public class Player {
 
     public int getMaxHealth() { return MAXHEALTH; }
 
-    /**
-     *
-     * @return current health total.
-     */
     public int getHealth() { return health; }
 
-    /**
-     * @param health The amount
-     */
     public void addHealth(int health) {
         setHealth(this.health+health);
+    }
+
+    public int getLifePoints() { return this.lifePoints; }
+
+    public boolean isAlive() { return getLifePoints()>0; }
+
+    public void resetPos() {
+        setxPos(getxBackup());
+        setyPos(getyBackup());
+    }
+
+    public int getObjective() {
+        return this.objective;
+    }
+
+    public void checkObjective(int ob) {
+        if (ob==this.objective) {
+            newBackup();
+            this.objective += 1;
+        }
+    }
+
+    public TextureRegion[][] setPlayerTextures (String texture) {
+        Texture playerTexture = new Texture(texture);
+        return TextureRegion.split(playerTexture, 300, 300);
+    }
+
+    public int cardsToSelect(){
+        return Math.min(5, getHealth() - 1);
+    }
+
+    public int lockedRegisters(){
+        return Math.max(0, 6-getHealth());
+    }
+
+    public void lockRegister(){
+        if (locked.size() < lockedRegisters()) {
+            locked.add(selected.get(selected.size() - 1));
+            selected.remove(selected.size() - 1);
+        } else if (locked.size() > lockedRegisters()) {
+            selected.add(locked.get(locked.size() - 1));
+            locked.remove(locked.size() - 1);
+        }
+        if (locked.size() != lockedRegisters()) {
+            lockRegister(); //TODO get rid of infinite loop?
+        }
+        assert locked.size() == lockedRegisters() : "Unexpected value: " + locked.size();
+    }
+
+    public void setHand(Deck deck) {
+        //The hand of the player.
+        Card[] playerHand = new Card[getHealth() - 1];
+        selected = new ArrayList<>();
+
+        for(int i = 0; i < playerHand.length; i++) {
+            if (deck.Cards.isEmpty()){ // shuffles when empty
+                deck.empty();
+            }
+            playerHand[i] = deck.Cards.poll();
+            assert playerHand[i] != null;
+            playerHand[i].setOwner(this);
+        }
+        Arrays.sort(playerHand);
+        this.cards = playerHand;
+    }
+
+    public Card[] getCards() {
+        return this.cards;
+    }
+
+    public void toggleCard(Card c) {
+        if (selected.contains(c)) {
+            selected.remove(c);
+            return;
+        }
+        selected.add(c);
+    }
+
+    public List<Card> getSelected(){
+        return selected;
+    }
+
+    public ArrayList<Card> getLocked(){
+        return locked;
+    }
+
+    public void discardDraw(Deck deck){
+        for (Card c : cards) {
+            if (locked.contains(c) && locked.indexOf(c) < lockedRegisters()) {
+                continue;
+            }
+            deck.Discard.add(c);
+            locked.remove(c);
+        }
+        setHand(deck);
+    }
+
+    public void respawn() {
+        if (getHealth() <= 0 && isAlive()){
+            removeLifePoint();
+            this.health = MAXHEALTH;
+        }
     }
 
     /**
@@ -263,167 +280,10 @@ public class Player {
     }
 
     /**
-     * Changes the lifePoints
+     * Subtracts one life point from the player
      */
     private void removeLifePoint() {
         this.lifePoints -= 1;
         if (isAlive()) resetPos();
-    }
-
-    /**
-     * @return the amount of lifePoints
-     */
-    public int getLifePoints() { return this.lifePoints; }
-
-    /**
-     * @return true if robot is alive
-     */
-    public boolean isAlive() { return getLifePoints()>0; }
-
-    public void resetPos() {
-        setxPos(getxBackup());
-        setyPos(getyBackup());
-    }
-
-    /**
-     * @return the next objective to stand at to get points
-     */
-    public int getObjective() {
-        return this.objective;
-    }
-
-    /**
-     * @param ob  new objective
-     */
-    public void checkObjective(int ob) {
-        if (ob==this.objective) {
-            newBackup();
-            this.objective += 1;
-        }
-    }
-
-    /**
-     * @param texture for the sprite of a player
-     * @return true if robot is alive
-     */
-    public TextureRegion[][] setPlayerTextures (String texture) {
-        Texture playerTexture = new Texture(texture);
-        return TextureRegion.split(playerTexture, 300, 300);
-    }
-
-    /**
-     * How many cards you may select in this round
-     * @return # of cards to select
-     */
-    public int cardsToSelect(){
-        return Math.min(5, getHealth() - 1);
-    }
-
-    /**
-     * How many registers are locked such that one cannot change the cards
-     * @return # of locked registers
-     */
-    public int lockedRegisters(){
-        return Math.max(0, 6-getHealth());
-    }
-
-    /**
-     * Lock the last selected card if damaged such that the register must lock
-     */
-    public void lockRegister(){
-        if (locked.size() < lockedRegisters()) {
-            locked.add(selected.get(selected.size() - 1));
-            selected.remove(selected.size() - 1);
-        } else if (locked.size() > lockedRegisters()) {
-            selected.add(locked.get(locked.size() - 1));
-            locked.remove(locked.size() - 1);
-        }
-        if (locked.size() != lockedRegisters()) {
-            lockRegister(); //TODO get rid of infinite loop?
-        }
-        assert locked.size() == lockedRegisters() : "Unexpected value: " + locked.size();
-    }
-
-    /**
-     * gives cards to player based on how much health is left
-     * @param deck used to get cards from
-     */
-    public void setHand(Deck deck) {
-        //The hand of the player.
-        Card[] playerHand = new Card[getHealth() - 1];
-        selected = new ArrayList<>();
-
-        for(int i = 0; i < playerHand.length; i++) {
-            if (deck.Cards.isEmpty()){ // shuffles when empty
-                deck.empty();
-            }
-            playerHand[i] = deck.Cards.poll();
-            assert playerHand[i] != null;
-            playerHand[i].setOwner(this);
-        }
-        Arrays.sort(playerHand);
-        this.cards = playerHand;
-    }
-
-    /**
-     *
-     * @return returns the cards the current player is holding
-     */
-    public Card[] getCards() {
-        return this.cards;
-    }
-
-    /**
-     * Toggles whether a card is selected or not
-     *
-     * @param c is the card we toggle
-     */
-    public void toggleCard(Card c) {
-        if (selected.contains(c)) {
-            selected.remove(c);
-            return;
-        }
-        selected.add(c);
-    }
-
-    /**
-     * Get the list of currently selected cards
-     * @return list of cards
-     */
-    public List<Card> getSelected(){
-        return selected;
-    }
-
-    /**
-     * Get the list of currently locked cards, this list will be in reverse order of execution
-     * @return list of locked cards
-     */
-    public ArrayList<Card> getLocked(){
-        return locked;
-    }
-
-    /**
-     * Discards all current cards except the ones locked, and draw a new hand
-     * @param deck deck to draw from and return the cards to
-     */
-    public void discardDraw(Deck deck){
-        for (Card c : cards) {
-            if (locked.contains(c) && locked.indexOf(c) < lockedRegisters()) {
-                continue;
-            }
-            deck.Discard.add(c);
-            locked.remove(c);
-        }
-        setHand(deck);
-    }
-
-    /**
-     * Respawn function to be called after each round, will set position to backup coords and remove 1 life
-     */
-    public void respawn() {
-        if (getHealth() <= 0 && isAlive()){
-            removeLifePoint();
-            this.health = MAXHEALTH;
-        }
     }
 }
